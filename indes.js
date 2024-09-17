2d8f6d2bc8922895178e243672706197d7a65e72
Dineshdiv1 <dineshkumard620@gmail.com>
2024-09-17 14:06:42 +0530

    first commit

---
+++ indes.js
@@ -0,0 +1,49 @@
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyYkKv9hQuttvPNbeenLuaLW9KfbTH0JC9ZKGTJDOY1SW8pdu8gxcpsFlpRT3UImsP9/exec'
+//         const form = document.forms['submit-to-google-sheet']
+//         const msg = document.getElementById("msg")
+      
+//         form.addEventListener('submit', e => {
+//           e.preventDefault()
+//           fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//             .then(response => {
+//                 msg.innerHTML = "Message sent Successfully"
+//                 setTimeout(function() {
+//                     msg.innerHTML = ""
+//                 },5000)
+//                 form.reset()
+//             })
+//             .catch(error => console.error('Error!', error.message))
+//         })
+ function myCert(){
+    alert("You are redirecting to New Tab Click OK to View Certificate");
+ }
+//     // var element = document.body;
+//     // element.classList.toggle("dark-mode")
+// 
+// let mybutton = document.getElementById("myBtn");
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//       mybutton.style.display = "block";
+//     } else {
+//       mybutton.style.display = "none";
+//     }
+//   }
+// function topFunction() {
+// document.body.scrollTop = 200; // For Safari
+// document.documentElement.scrollTop = 2000; // For Chrome, Firefox, IE and Opera
+// }
+
+let friuts = ['orange','apple.','banana.']
+// friuts.forEach(item=>console.log(item))
+for(item of friuts){
+    console.log(item)
+}
+console.log(friuts)
+
+var num=0
+while(num<6)
+{
+    console.log(num)
+    num++
+}
